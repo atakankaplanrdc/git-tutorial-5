305d093564426edec55ce6e70eb4c5c5479120c0
atakankaplann <atakan.kaplan@rdc.com.tr>
2025-11-12 23:41:13 +0300

    This is an example commit for master before conflixt branch

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 console.log("This is version - 3.2");
 console.log("This is a new feature too!!!");
 console.log("Features are done in this feature1 branch!");
+console.log("This is an example text for master branch");
