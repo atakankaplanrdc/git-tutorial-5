1d3127ee021ec641d32ce2253ca3ef12f079c68b
atakankaplann <atakan.kaplan@rdc.com.tr>
2025-11-15 00:22:52 +0300

    This is feature-2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 console.log("This is version - 3.2");
 console.log("This is a new feature too!!!");
 console.log("Features are done in this feature2 branch!");
-console.log("bugfix solved");
+console.log("feature-2");
